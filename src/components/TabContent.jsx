42bb92b0d0828fd5eaf80680b9fa694b3be217eb
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-11 12:04:44 +0800

    Refactor TabContent component and add new functionality
    - Refactor the TabContent component to improve code readability and indentation
    - Add new functionality to handle triple increment of likes and undo actions

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -8,6 +8,21 @@ const TabContent = ({ item }) => {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -21,13 +36,13 @@ const TabContent = ({ item }) => {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
